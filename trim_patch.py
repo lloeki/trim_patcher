8d5fe15f1c6865cfce25ed2d7fafb7364da9866a
Loic Nageleisen <loic.nageleisen@gmail.com>
2014-09-23 17:30:07 +0200

    10.9.4 support

--- trim_patch.py
+++ trim_patch.py
@@ -33,6 +33,7 @@ md5_version = {
     "8a6e253e621db78e1fa0d14274ade63c": ["10.9"],
     "a11100b558cade33c35924f08924a728": ["10.9.2"],
     "3e794ede0ec1becce65c580a6efceaca": ["10.9.3"],
+    "2a8c2b865eba74fe0c5ca8bfa9f7d603": ["10.9.4"],
 }
 md5_patch = {
     "25a29cbdbb89329a6ce846c9b05af5f0": "d76b57daf4d4c2ff5b52bc7b4b2dcfc1",
@@ -51,12 +52,18 @@ md5_patch = {
     "8a6e253e621db78e1fa0d14274ade63c": "17dc2e1edc35447ea693a29aef969f21",
     "a11100b558cade33c35924f08924a728": "3409ad454e58f6415c38ab2b9e39d64d",
     "3e794ede0ec1becce65c580a6efceaca": "3ae907c12b58e2e682e215627c87be67",
+    "2a8c2b865eba74fe0c5ca8bfa9f7d603": "9a254024a1c43a102a5c558a6510413e",
 }
 md5_patch_r = dict((v, k) for k, v in md5_patch.items())
 
 re_index = [
     {
         'search': ("(\x52\x6F\x74\x61\x74\x69\x6F\x6E\x61\x6C"
+                   "\x00{1,20})[^\x00]{9}(\x00{1,20}[^\x00])"),
+        'replace': "\\1\x00\x00\x00\x00\x00\x00\x00\x00\x00\\2"
+    },
+    {
+        'search': ("(\x4F\x6E\x57\x61\x6B\x65\x4B\x65\x79\x0A"
                    "\x00{1,20})[^\x00]{9}(\x00{1,20}[^\x00])"),
         'replace': "\\1\x00\x00\x00\x00\x00\x00\x00\x00\x00\\2"
     },
@@ -79,6 +86,9 @@ re_md5 = {
         "8a6e253e621db78e1fa0d14274ade63c",
         "a11100b558cade33c35924f08924a728",
         "3e794ede0ec1becce65c580a6efceaca",
+        ],
+    1: [
+        "2a8c2b865eba74fe0c5ca8bfa9f7d603",
         ],
 }
 md5_re = dict((v, re_index[k]) for k, l in re_md5.items() for v in l)
